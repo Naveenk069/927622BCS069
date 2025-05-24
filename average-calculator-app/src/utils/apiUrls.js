65d11a1d02ce4e59cb98cbf96caca005a7f2fab0
NAVEENKUMAR M <mnaveenk2004@gmail.com>
2025-05-24 12:09:45 +0530

    The app has been created

--- average-calculator-app/src/utils/apiUrls.js
+++
@@ -1,6 +0,0 @@
-export const API_URLS = {
-  p: "http://20.244.56.144/evaluation-service/primes",
-  f: "http://20.244.56.144/evaluation-service/fibo",
-  e: "http://20.244.56.144/evaluation-service/even",
-  r: "http://20.244.56.144/evaluation-service/rand"
-};
--- average-calculator-app/src/utils/averageUtils.js
+++
@@ -1,6 +0,0 @@
-export const calculateAverage = (arr) => {
-  if (arr.length === 0) return 0;
-  const sum = arr.reduce((acc, num) => acc + num, 0);
-  return parseFloat((sum / arr.length).toFixed(2));
-};
- 
